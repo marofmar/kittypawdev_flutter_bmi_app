562bd9b5d3bdf9006403814fc6ab2606116208b7
Yujin <yujin.mar.chung@gmail.com>
2024-11-13 13:59:03 +0900

    feat(main): 다크,라이트테마 적용할 바탕 만듦

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bmi_app/pages/home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        themeMode: ThemeMode.light,
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.pinkAccent, brightness: Brightness.light)),
+        darkTheme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.pinkAccent, brightness: Brightness.dark)),
+        home: HomePage());
+  }
+}
